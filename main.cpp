84d822bb521616f51b1744c792507cc6cc71a34b
LukaszWachala <67100189+LukaszWachala@users.noreply.github.com>
2020-06-18 14:11:31 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -6,15 +6,18 @@ using namespace std;
 
 int main()
 {
-    pole p1;
-    objetosc p2;
+    pole p;
+    objetosc o;
     double a, b ,h;
     cout<<"Podaj dlugosc a: "; cin>>a;
     cout<<"Podaj dlugosc b: "; cin>>b;
     cout<<"Podaj dlugosc H: "; cin>>h;
-
-    p1.oblicz(a,b);
-    p2.oblicz(a,b,h);    
+    
+    cout<<"Prostokat(podstawa)" <<endl;
+    p.oblicz(a,b);
+    cout<<"Prostopadloscian" <<endl;
+    p.oblicz_siatke(a,b,h);
+    o.oblicz(a,b,h);    
 
     return 0;
 }
