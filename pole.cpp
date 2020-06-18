b2af9a0da14b307fb42759ecb0e05abacecd8ef1
LukaszWachala <67100189+LukaszWachala@users.noreply.github.com>
2020-06-18 14:08:39 +0200

    Update pole.cpp

--- pole.cpp
+++ pole.cpp
@@ -8,3 +8,9 @@ void pole::oblicz(double a, double b)
     wynik=a*b;
     cout <<"Pole: "<<wynik <<endl;
 }
+
+void pole::oblicz_siatke(double a, double b, double h)
+{
+    wynik=2*a*b+2*a*h+2*b*h;
+    cout <<"Pole siatki: "<<wynik <<endl;
+}
